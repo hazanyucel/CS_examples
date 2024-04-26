8069b02c9ba2381d7f38eef07bada9e1c3d8866b
Hazan Yücel <118287430+hazanyucel@users.noreply.github.com>
2024-04-26 10:03:47 +0300

    Add files via upload
    Object Oriented Programming Homework

---
+++ Odev/Odev/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace Odev
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.karekok = new System.Windows.Forms.Button();
+            this.max = new System.Windows.Forms.Button();
+            this.mutlak = new System.Windows.Forms.Button();
+            this.sin = new System.Windows.Forms.Button();
+            this.cps = new System.Windows.Forms.Button();
+            this.gerial = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.çiz = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // karekok
+            // 
+            this.karekok.BackColor = System.Drawing.Color.RosyBrown;
+            this.karekok.Location = new System.Drawing.Point(155, 118);
+            this.karekok.Name = "karekok";
+            this.karekok.Size = new System.Drawing.Size(75, 41);
+            this.karekok.TabIndex = 0;
+            this.karekok.Text = "karekök";
+            this.karekok.UseVisualStyleBackColor = false;
+            this.karekok.Click += new System.EventHandler(this.karekok_Click);
+            // 
+            // max
+            // 
+            this.max.BackColor = System.Drawing.Color.RosyBrown;
+            this.max.Location = new System.Drawing.Point(260, 118);
+            this.max.Name = "max";
+            this.max.Size = new System.Drawing.Size(75, 41);
+            this.max.TabIndex = 1;
+            this.max.Text = "max";
+            this.max.UseVisualStyleBackColor = false;
+            this.max.Click += new System.EventHandler(this.max_Click);
+            // 
+            // mutlak
+            // 
+            this.mutlak.BackColor = System.Drawing.Color.RosyBrown;
+            this.mutlak.Location = new System.Drawing.Point(54, 118);
+            this.mutlak.Name = "mutlak";
+            this.mutlak.Size = new System.Drawing.Size(75, 41);
+            this.mutlak.TabIndex = 2;
+            this.mutlak.Text = "mutlak";
+            this.mutlak.UseVisualStyleBackColor = false;
+            this.mutlak.Click += new System.EventHandler(this.mutlak_Click);
+            // 
+            // sin
+            // 
+            this.sin.BackColor = System.Drawing.Color.RosyBrown;
+            this.sin.Location = new System.Drawing.Point(54, 165);
+            this.sin.Name = "sin";
+            this.sin.Size = new System.Drawing.Size(75, 39);
+            this.sin.TabIndex = 3;
+            this.sin.Text = "sin";
+            this.sin.UseVisualStyleBackColor = false;
+            this.sin.Click += new System.EventHandler(this.sin_Click);
+            // 
+            // cps
+            // 
+            this.cps.BackColor = System.Drawing.Color.RosyBrown;
+            this.cps.Location = new System.Drawing.Point(155, 165);
+            this.cps.Name = "cps";
+            this.cps.Size = new System.Drawing.Size(75, 39);
+            this.cps.TabIndex = 4;
+            this.cps.Text = "cos";
+            this.cps.UseVisualStyleBackColor = false;
+            this.cps.Click += new System.EventHandler(this.cps_Click);
+            // 
+            // gerial
+            // 
+            this.gerial.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.gerial.Location = new System.Drawing.Point(260, 207);
+            this.gerial.Name = "gerial";
+            this.gerial.Size = new System.Drawing.Size(75, 33);
+            this.gerial.TabIndex = 5;
+            this.gerial.Text = "Geri Al";
+            this.gerial.UseVisualStyleBackColor = false;
+            this.gerial.Click += new System.EventHandler(this.gerial_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(21, 31);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(21, 85);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(51, 20);
+            this.checkBox2.TabIndex = 9;
+            this.checkBox2.Text = "cos";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(21, 59);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(46, 20);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "sin";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // çiz
+            // 
+            this.çiz.BackColor = System.Drawing.Color.DarkKhaki;
+            this.çiz.Location = new System.Drawing.Point(149, 70);
+            this.çiz.Name = "çiz";
+            this.çiz.Size = new System.Drawing.Size(93, 23);
+            this.çiz.TabIndex = 10;
+            this.çiz.Text = "çiz";
+            this.çiz.UseVisualStyleBackColor = false;
+            this.çiz.Click += new System.EventHandler(this.çiz_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(149, 31);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(153, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "bitiş";
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.Khaki;
+            this.textBox3.Font = new System.Drawing.Font("Mongolian Baiti", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(54, 76);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(281, 39);
+            this.textBox3.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(257, 165);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(147, 16);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "*a,b şeklinde giriniz";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.çiz);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Location = new System.Drawing.Point(54, 246);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(260, 115);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "ÇİZİM";
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(54, 367);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(281, 122);
+            this.panel1.TabIndex = 14;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(392, 512);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.gerial);
+            this.Controls.Add(this.cps);
+            this.Controls.Add(this.sin);
+            this.Controls.Add(this.mutlak);
+            this.Controls.Add(this.max);
+            this.Controls.Add(this.karekok);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button karekok;
+        private System.Windows.Forms.Button max;
+        private System.Windows.Forms.Button mutlak;
+        private System.Windows.Forms.Button sin;
+        private System.Windows.Forms.Button cps;
+        private System.Windows.Forms.Button gerial;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button çiz;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Odev/Odev/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.AxHost;
+
+namespace Odev
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mutlak_Click(object sender, EventArgs e)
+        {
+            double sayi = Convert.ToDouble(textBox3.Text);
+            if (sayi < 0)
+            {
+                sayi = sayi * (-1);
+                textBox3.Text = Convert.ToString(sayi);
+
+            }
+            else
+                textBox3.Text = Convert.ToString(sayi);
+        }
+
+        private void karekok_Click(object sender, EventArgs e)
+        {
+            // girilen sayının karekökünü, 1 ve sayı arasındaki orta değere bakarak bulmaya çalışıyor
+            
+
+            double sayi = Convert.ToDouble(textBox3.Text);
+            double epsilon = 0.00001; // Hesaplama hassasiyeti
+            double baslangic = 0;
+            double son = Math.Max(1, sayi);
+            double orta = (baslangic + son) / 2;
+
+            while (son - baslangic > epsilon)
+            {
+                orta = (baslangic + son) / 2;
+                if (orta * orta > sayi)
+                    son = orta;
+                else
+                    baslangic = orta;
+            }
+            textBox3.Text = Convert.ToString(orta);
+        }
+
+        private void max_Click(object sender, EventArgs e)
+        {
+            double max = -1;
+            if (!string.IsNullOrEmpty(textBox3.Text))
+            {
+                
+                string[] numbers = textBox3.Text.Split(',');
+                double[] doublenum = new double[numbers.Length];
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    double sayi= Convert.ToDouble(numbers[i]);
+                    
+                    doublenum[i] = sayi;
+
+                }
+                for (int i = 0; i < doublenum.Length-1; i++)
+                {
+                    if (doublenum[i] < doublenum[i+1])
+                    {
+                        max = doublenum[i + 1];
+                    }
+                    
+                }
+
+            }
+            textBox3.Text= max.ToString();
+        }
+
+        private void sin_Click(object sender, EventArgs e)
+        {
+            double aci =Convert.ToDouble(textBox3.Text);  
+            double sinus=Math.Sin((Math.PI * aci) / 180.0);
+            textBox3.Text = Convert.ToString(sinus);
+        }
+
+        private void cps_Click(object sender, EventArgs e)
+        {
+            double aci = Convert.ToDouble(textBox3.Text);
+            double cosinus=Math.Cos((Math.PI * aci) / 180.0);
+            textBox3.Text = Convert.ToString(cosinus);
+        }
+
+        private void çiz_Click(object sender, EventArgs e)
+        {
+            
+            // Başlangıç ve bitiş değerlerini al
+            double baslangic = Convert.ToDouble(textBox1.Text);
+            double bitis = Convert.ToDouble(textBox2.Text);
+
+            // Grafiği çizmek için Graphics nesnesi oluştur
+            Graphics g = panel1.CreateGraphics();
+            Pen sinPen = new Pen(Color.Blue);
+            Pen cosPen = new Pen(Color.Red);
+
+            // Panel boyutları
+            int width = panel1.Width;
+            int height = panel1.Height;
+            int centerX = width / 2;
+            int centerY = height / 2;
+
+            // Ölçek
+            double scale = 40;
+            g.DrawLine(Pens.Black, 0, centerY, width, centerY); // X ekseni
+            g.DrawLine(Pens.Black, centerX, 0, centerX, height); // Y ekseni
+
+            // Başlangıçtan bitişe kadar adım adım grafiği çiz
+            for (double x = baslangic; x <= bitis; x += 0.1)
+            {
+                if (checkBox1.Checked)
+                {
+                    double sinValue = Math.Sin(x);
+                    double sinY = centerY - sinValue * scale;
+                    g.DrawLine(sinPen, (float)(x * scale) + centerX, (float)sinY, (float)(x * scale) + centerX, (float)sinY + 1);
+                }
+
+
+                if (checkBox2.Checked)
+                {
+                    double cosValue = Math.Cos(x);
+                    double cosY = centerY - cosValue * scale;
+                    g.DrawLine(cosPen, (float)(x * scale) + centerX, (float)cosY, (float)(x * scale) + centerX, (float)cosY + 1);
+                }
+                
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void gerial_Click(object sender, EventArgs e)
+        {
+            textBox3.Text= string.Empty;
+            panel1.Visible = false;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
